cd43fd9e2ed5d5585f534fdc68493a69dc422c06
Yespecom <yespecomstudio@gmail.com>
2025-07-19 23:20:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors") // Import the cors package
 const categoryRoutes = require("./routes/categoryRoutes")
 const { connectMainDB } = require("./config/db") // Import connectMainDB
 
@@ -8,6 +9,7 @@ const app = express()
 // Connect to the main database on server start (optional for category service, but good practice if it needs to interact with main DB)
 connectMainDB()
 
+app.use(cors()) // Use cors middleware to enable CORS for all routes
 app.use(express.json()) // For parsing application/json
 app.use("/api/categories", categoryRoutes)
 
